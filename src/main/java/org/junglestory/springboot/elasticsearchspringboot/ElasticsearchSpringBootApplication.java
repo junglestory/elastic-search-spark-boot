aaa58ef10e1976ac7eb70ef0ebe90ceca80bcbc6
김덕규 <monocaine@gmail.com>
2018-07-04 22:39:47 +0900

    Initial project

---
+++ src/main/java/org/junglestory/springboot/elasticsearchspringboot/ElasticsearchSpringBootApplication.java
@@ -0,0 +1,12 @@
+package org.junglestory.springboot.elasticsearchspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/org/junglestory/springboot/elasticsearchspringboot/ElasticsearchSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package org.junglestory.springboot.elasticsearchspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
